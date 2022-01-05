b450a0f2c1b7f474ebddbd782980988f6d364356
Nacho <novozhilovanton@yandex.ru>
2022-01-05 12:00:40 +0300

    delete \ and add in gitignore

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def test_task_1():
     expected = 'one, two, three, and four'
     list_for_check = ['one', 'two', 'three', 'four']
     fact = tasks.transform_list_join(list_for_check)
-    print(f'Result: {expected == fact}\n\
-        Str_fact: {fact}\n\
-        Str_expected: {expected}')
+    print(f'Result: {expected == fact}\n'
+          f'Str_fact: {fact}\n'
+          f'Str_expected: {expected}')
 
 
 if __name__ == '__main__':
